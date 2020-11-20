6fa17b792437a489c80696ddf86c88b76cce45da
sioncen <52478851+sioncen@users.noreply.github.com>
2020-11-20 16:40:14 +0800

    Update test.py
    ...

--- bin/test.py
+++ bin/test.py
@@ -1,20 +1,27 @@
 from __future__ import absolute_import
 
 import os
+import time
 from got10k.experiments import *
+
+import multiprocessing
+multiprocessing.set_start_method('spawn',True)
 
 from siamfc import TrackerSiamFC
 
 
 if __name__ == '__main__':
-    net_path = 'models/siamfc_alexnet_e50.pth'# 
+    begin = time.time()
+    start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(begin))
+    print(">>>测试起始时间:{}".format(start))
+    net_path = 'pretrained/'# 
     tracker = TrackerSiamFC(net_path=net_path) #初始化一个追踪器
 
     # root_dir = os.path.abspath('datasets/OTB')
     # e = ExperimentOTB(root_dir, version=2013)
 
     root_dir = os.path.abspath('datasets/OTB')
-    e = ExperimentOTB(root_dir, version=2015)
+    e = ExperimentOTB(root_dir, version=2013)
 
     # root_dir = os.path.abspath('datasets/UAV123')
     # e = ExperimentUAV123(root_dir, version='UAV123')
@@ -49,3 +56,13 @@ if __name__ == '__main__':
     e.run(tracker,visualize=False)#默认不开启可视化
 
     e.report([tracker.name])
+
+    end = time.time()
+    stop = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end))
+    time_used = (end - begin)/60
+    time_used_t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_used))
+    print(net_path)
+    print(">>>测试起始时间:{}".format(start))
+    print(">>>测试结束时间:{}".format(stop))
+    print(">>>测试用时:{}分钟".format(time_used))
+    print("================================================================================================================================")
